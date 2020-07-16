3e025e39d9f3a5537ad82a3239d0e3242a9af0d1
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2020-07-16 11:32:13 +0000

    Update DotProduct.py

--- DotProduct.py
+++ DotProduct.py
@@ -8,22 +8,22 @@ import random
 N=10
 A=[];
 for i in range(1,N):
-    fill =random.randint(1,1000)
-    A.append(fill)
+    fill =random.randint(1,1000) #takes numbers at random from 1 to N
+    A.append(fill) # adds number picked to the list
 
 B=[];
 for j in range(1,N):
-    fill2 =random.randint(1,1000)
-    B.append(fill2)
+    fill2 =random.randint(1,1000) #takes numbers at random from 1 to N
+    B.append(fill2) # adds number picked to the list
 
 
 
     
 def DotProduct(N,A,B):
-    product =0
-    for i in range(len(A)):
-        product+= A[i]* B[i]
-    return product
+    product =0 #holds total number
+    for i in range(N):
+        product+= A[i]* B[i] #multiplies the index of A and B then adds to product
+    return product           # repeats this process till the end of the loop
 
 def main():
     answer = DotProduct(N,A,B)
